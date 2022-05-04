8f5d7fc9642e56c8a322ba1161b64a4f7e9321c0
erfanhanifezade <erfanhanifezade@gmail.com>
2022-05-04 15:33:09 +0430

    home page

---
+++ lib/src/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz App'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).backgroundColor,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage('images/welcome.png')),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'START GAME',
+                style: TextStyle(fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).primaryColor,
+                padding: EdgeInsets.all(16),
+                elevation: 5,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
